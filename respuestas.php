d5a544cea84269e6a7b34c0d3845925b4fabc3ca
HCamberos <56201325+HCamberos@users.noreply.github.com>
2022-05-25 09:58:01 -0500

    Add files via upload

---
+++ respuestas.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo $_POST["radp1"];
+echo "<br>"; 
+echo $_POST["radp2"];
+echo "<br>"; 
+echo $_POST["radp3"];
+echo "<br>"; 
+echo $_POST["p4"];
+echo "<br>"; 
+echo $_POST["p5"];
+echo "<br>"; 
+echo $_POST["p6"];
+echo "<br>"; 
+echo $_POST["p7"];
+echo "<br>"; 
+echo $_POST["p8"];
+
+?>
